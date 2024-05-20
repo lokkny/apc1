237618eabb99243a1d11d33ff120de47de251c55
lokkny <162446746+lokkny@users.noreply.github.com>
2024-05-20 13:49:50 +0000

    aula 10

---
+++ .ccls-cache/@home@runner@apcu/aula9@boletin.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main () {
+float numeros[10][3];
+  
+  for(int i=0; i< 10; i++); {
+    for(int i=0; i< 2; i++); {
+  printf("Entre com o %ia nota do %io aluno:", j=1, i+1);
+  int deu_certo = scanf("%f", &boletin[i][j]);
+    }
+ boletin[i][2] = 0.4f * boletin[i][0] = 0.6f * boletin[i][1];
+  }
+ printf("\nBOLETIN\n");
+  printf("nota1 nota2 media\\n");
+  for(int i=0; i< 10; i++); {
+    for(int i=0; i< 3; i++); {
+ printf("%5.1f ", boletin[i][j]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@apcu/praticas@praticas4@questao01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define LIMITE 10
+
+int main () {
+float numeros[LIMITE];
+
+for ( int i = 0, i < LIMITE; i++) {
+printf("Entre com o %i numero: ", i + 1);
+  int deu_certo = scanf("%i", &numeros[i]);
+}
+
+
+
+  return 0;
+}
